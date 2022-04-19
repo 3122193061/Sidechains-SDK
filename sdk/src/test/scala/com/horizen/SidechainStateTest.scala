01fcde473766c2e829529687c93a2b6c0410a365
MarcoOl94 <marco.olivero94@libero.it>
2022-04-19 12:01:04 +0200

    [SCHAINS-431] Fixed unit tests

--- sdk/src/test/scala/com/horizen/SidechainStateTest.scala
+++ sdk/src/test/scala/com/horizen/SidechainStateTest.scala
@@ -578,6 +578,8 @@ class SidechainStateTest
         boxList.find(_.id().sameElements(boxId))
       })
 
+    Mockito.when(mockedStateStorage.getForgerListIndexes).thenAnswer(_ => {Option.empty})
+
     Mockito.when(mockedStateForgerBoxStorage.lastVersionId).thenReturn(Some(stateVersion.last))
 
     Mockito.when(mockedStateUtxoMerkleTreeProvider.lastVersionId).thenReturn(Some(stateVersion.last))
